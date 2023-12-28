7e4327af239a51258fdece4760fcf48797c715cb
AksharaBhanu <javagalbhanu@gmail.com>
2023-12-28 09:16:44 +0530

    Day19

---
+++ B93/src/day19b/Demo6.java
@@ -0,0 +1,30 @@
+package day19b;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo6 {
+//valid login-logout
+	public static void main(String[] args) {
+		
+			WebDriver driver=new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+			driver.manage().window().maximize();
+			driver.get("https://pos.aksharatraining.in/pos/public/");
+			
+			LoginPage loginpage=new LoginPage(driver);
+			loginpage.setUserName("admin");
+			loginpage.setPassword("pointofsale");
+			loginpage.clickGoButton();
+			
+			HomePage homepage=new HomePage(driver);
+			homepage.clickLogout();
+			
+			driver.quit();
+		}
+
+
+}
